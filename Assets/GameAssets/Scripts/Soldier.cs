a81bbdcdeaa7453fcc49ec2a52fa9f562aa303b8
Saix_93 <r.torresgonzalez.93@gmail.com>
2017-05-16 22:28:34 +0200

    upload project

---
+++ Assets/GameAssets/Scripts/Soldier.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soldier : MonoBehaviour
+{
+
+    // Puntos de vida actuales del soldado, si llega a 0 o menos, el soldado muere
+    public float life = 100;
+
+    // Puntería del soldado, entre 0 y 1
+    public float accuracy = 0.5f;
+
+    // Probabilidad de crítico entre 0 y 1
+    // [System.NonSerialized] --> Pública solo para scripts, no aparece en el inspector
+    public float criticalChance = 0.05f;
+
+    // Referencia al arma del soldado
+    // [SerializeField] --> Muestra el campo en el inspector, incluso si es privado
+    public Weapon weapon;
+
+    // Soldado objetivo
+    public Soldier target;
+
+    // Nombre del soldado enemigo
+    public string targetName;
+
+    // El soldado está vivo
+    public bool isAlive = true;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        Soldier target = GameObject.Find(targetName).GetComponent<Soldier>();
+        AimAtTarget(target);
+
+        //while(life > 0 && target.life > 0)
+        //{
+        //    ShootAtTarget(target);
+        //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isAlive && target.isAlive)
+        {
+            // Si aún tenemos munición
+            if (weapon.currentAmmo > 0)
+            {
+                // Disparamos al objetivo
+                ShootAtTarget(target);
+            }
+            else
+            {
+                // Si no, recargamos el arma
+                ReloadWeapon();
+            }
+        }
+    }
+
+    private void AimAtTarget(Soldier target)
+    {
+        // Miramos al objetivo
+        transform.LookAt(target.transform);
+    }
+
+    private void ShootAtTarget(Soldier target)
+    {
+        // Se dispara al objetivo teniendo en cuenta las tiradas de puntería y crítico
+        weapon.ShootAtTarget(this.name, target, accuracy, criticalChance);
+    }
+
+    private void ReloadWeapon()
+    {
+        // Llamamos al método Reload del arma para que recargue
+        weapon.Reload();
+    }
+
+    public void GetDamage(float damage)
+    {
+        // Restamos vida según el golpe que hemos recibido
+        life = life - damage;
+
+        if (life <= 0)
+        {
+            isAlive = false;
+        }
+    }
+}
---
+++ Assets/GameAssets/Scripts/Weapon.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+    // Daño del arma por disparo
+    public float damage = 1;
+
+    // Daño que hace el arma por disparo crítico
+    public float criticalDamage = 2;
+
+    // Munición total del arma
+    // public int maxAmmo;
+    // Munición actual del arma
+    [System.NonSerialized]
+    public int currentAmmo;
+    // Munición máxima por cargador
+    public int maxClipAmmo;
+
+    // Cadencia del arma
+    // Tiempo por cada disparo
+    public float firerate = 0.4f;
+
+    // Tiempo que el soldado tarda en recargar el arma
+    public float reloadTime = 0.5f;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        currentAmmo = maxClipAmmo;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ShootAtTarget(string soldierName, Soldier target, float accuracy, float criticalChance)
+    {
+        currentAmmo--;
+
+        if (Random.value <= accuracy)
+        {
+            if (Random.value <= criticalChance)
+            {
+                target.GetDamage(criticalDamage);
+                Debug.Log("¡Golpe crítico!: " + target.name + " se queda con " + target.life + " puntos de vida");
+            }
+            else
+            {
+                target.GetDamage(damage);
+                Debug.Log("Golpe normal: " + target.name + " se queda con " + target.life + " puntos de vida");
+            }
+
+            if (target.life <= 0)
+            {
+                Debug.Log(soldierName + " ha matado a " + target.name);
+            }
+        }
+        else
+        {
+            Debug.Log(soldierName + " ha fallado el disparo");
+        }
+    }
+
+    public void Reload()
+    {
+        Debug.Log("Recargando " + this.name + "...");
+        currentAmmo = maxClipAmmo;
+    }
+}
